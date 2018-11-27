12c7a0393ac64ce28d4e75b24a62bc29e4a3859d
syilviawkp <sylviaputri0@gmail.com>
2018-11-27 10:22:00 +0700

    button signup

--- tubes/app/src/main/java/com/example/asus/cookfun/Register.java
+++ tubes/app/src/main/java/com/example/asus/cookfun/Register.java
@@ -1,13 +1,27 @@
 package com.example.asus.cookfun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
+
+    Button button_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        button_signup = findViewById(R.id.button_signup);
+        button_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(Register.this, Home.class);
+                startActivity(a);
+            }
+        });
     }
 }
