494aecc0f65d4277e6dd56a7b85cdac17d5e4f75
syilviawkp <sylviaputri0@gmail.com>
2018-11-28 18:55:20 +0700

    register

--- tubes/app/src/main/java/com/example/asus/cookfun/Register.java
+++ tubes/app/src/main/java/com/example/asus/cookfun/Register.java
@@ -1,27 +1,51 @@
 package com.example.asus.cookfun;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.asus.cookfun.Model.DataHelper;
+import com.example.asus.cookfun.Session.SessionManagement;
 
 public class Register extends AppCompatActivity {
 
     Button button_signup;
+
+    DataHelper dbHelper;
+    EditText edtNama, edtEmail, edtPassword;
+    Cursor cursor;
+    SessionManagement sessionManagement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        dbHelper = new DataHelper(this);
+
+        edtNama = findViewById(R.id.edtNama);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
         button_signup = findViewById(R.id.button_signup);
+
+
         button_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent a = new Intent(Register.this, Home.class);
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.execSQL("INSERT INTO user(nama, email, password) VALUES ('"+edtNama.getText().toString()+"','"+edtEmail.getText().toString()+"','"+edtPassword.getText().toString()+"')");
+                Toast.makeText(getApplicationContext(), "Register Successful", Toast.LENGTH_SHORT).show();
+
+                Intent a = new Intent(Register.this, Login.class);
                 startActivity(a);
             }
         });
+
     }
 }
