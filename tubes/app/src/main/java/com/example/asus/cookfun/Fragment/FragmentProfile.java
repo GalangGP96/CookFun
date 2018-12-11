c56064ab4352078c850bdcdbe5077552d43ee4db
hendra <hendrapermana089@gmail.com>
2018-12-11 09:27:24 +0700

    profil

--- tubes/app/src/main/java/com/example/asus/cookfun/Fragment/FragmentProfile.java
+++ tubes/app/src/main/java/com/example/asus/cookfun/Fragment/FragmentProfile.java
@@ -7,13 +7,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.asus.cookfun.R;
+import com.example.asus.cookfun.Session.SessionManagement;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
 
 public class FragmentProfile extends Fragment {
+    ImageView logout;
+    SessionManagement sessionManagement;
+    TextView nama,email;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+        View view = inflater.inflate(R.layout.fragment_profile,container,false);
+        logout = (ImageView) view.findViewById(R.id.imgLogout);
+        nama = (TextView) view.findViewById(R.id.textNamaUser);
+        email = (TextView) view.findViewById(R.id.textEmailUser);
+
+        HashMap<String, String> user=sessionManagement.getUserInformation();
+        nama.setText("nama : "+user.get("nama"));
+        email.setText("email : "+user.get("email"));
+
+        sessionManagement = new SessionManagement(getContext());
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sessionManagement.logoutUser();
+            }
+        });
+
+
+
+
+        return view;
     }
 }
